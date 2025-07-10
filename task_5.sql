2937858042b28fcdb121abed04e84ecbaeef243c
donni2312 <a.donnelly2312@gmail.com>
2025-07-10 22:34:28 +0200

    add task3.sql

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+-- A script that inserts a single row into the Customers table.
+-- The database name will be passed as an argument of the mysql command.
+
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
